62db0659cef6b4a40c1cd8f6729cde22587fe004
Владимир Раду <rodny_co@list.ru>
2021-07-03 17:40:33 +0300

    Config settings

--- public/index.php
+++ public/index.php
@@ -2,3 +2,11 @@
 declare(strict_types=1);
 
 require __DIR__ . '../../src/App/App.php';
+
+/** @var \Slim\Factory\AppFactory $app */
+try {
+    $app->run();
+} catch (Exception $e) {
+    // We display a error message
+    die( json_encode(array("status" => "failed", "message" => "This action is not allowed")));
+}
--- src/App/App.php
+++ src/App/App.php
@@ -1,6 +1,16 @@
 <?php
 declare(strict_types=1);
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
 require __DIR__ . '../../../vendor/autoload.php';
 
-echo "app is run";
+$app = AppFactory::create();
+
+$app->get('/foo', function (Request $request, Response $response, array $args) {
+    $payload = json_encode(['hello' => 'world'], JSON_PRETTY_PRINT);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
+});
